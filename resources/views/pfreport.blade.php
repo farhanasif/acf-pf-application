f776b1e2d9e9aba3bc21c03dc3051f041a9608a8
Md Farhan Asif <f.asif2010@gmail.com>
2020-02-26 13:39:24 +0600

    report design fix

--- resources/views/pfreport.blade.php
+++ resources/views/pfreport.blade.php
@@ -12,6 +12,8 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- overlayScrollbars -->
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset('theme/plugins/datatables-bs4/css/dataTables.bootstrap4.css') }}">
   <link rel="stylesheet" href="{{ asset('theme/dist/css/adminlte.min.css') }}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
