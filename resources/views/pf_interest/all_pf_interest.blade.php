454345fbf0891baf95842ee9b72e5b489e70e122
Entertech <riponahmed2201@gmail.com>
2020-03-05 18:12:56 +0600

    pf interest delete problem solve

--- resources/views/pf_interest/all_pf_interest.blade.php
+++ resources/views/pf_interest/all_pf_interest.blade.php
@@ -7,7 +7,7 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>All PF Interest Fund </h1>
+        <h1>All PF Interest Information </h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
