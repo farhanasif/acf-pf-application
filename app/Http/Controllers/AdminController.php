36c8dc5d005fe4a7c963533e8d80c1cce9721050
ripon <riponahmed2201@gmail.com>
2020-06-10 13:14:06 +0600

    total balance and investment problem solve

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
     //  exit;
 
         $data['total_balance_in_account_from_pf_deposit'] = DB::table('pf_deposit')->sum('total_pf');
-        $data['total_balance_in_account_from_transaction'] = DB::table('pf_deposit')->sum('total_pf');
+        $data['total_balance_in_account_from_transaction'] = DB::table('transactions')->sum('amount');
 
         $data['total_employees'] = DB::table("employees")->count('staff_code');
         $data['total_loans'] = DB::select('SELECT COUNT(DISTINCT staff_code) AS total_loan, SUM(loan_amount) AS total_loan_amount FROM loans');
