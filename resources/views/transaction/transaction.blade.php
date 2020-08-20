0120a86fe3a00b00c3fcfb013456db50a0b4d6b1
Farhan <f.asif2010@gmail.com>
2020-08-20 13:57:52 +0600

    ok

--- resources/views/transaction/transaction.blade.php
+++ resources/views/transaction/transaction.blade.php
@@ -9,7 +9,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Bank Reconciliation</h1>
+                    <h1>Bank Transactions</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
